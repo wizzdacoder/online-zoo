224a5520b4ca6aeef8286659b4fa873fe694607f
Alan Demian Beltramo <beltramoalan@gmail.com>
2022-12-01 03:35:33 -0300

    feat: 1000px render and some 640px

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
 "use strict"
 
+function changeImage() {
 
+    if (document.getElementsByClassName("leftarrow").src=="../../assets/images/arrow_elips_1.png")
+    {
+        document.getElementsByClassName("leftarrow").src="../../assets/icons/arrow_elips_l.png";
+
+    }
+}
 
 
 
